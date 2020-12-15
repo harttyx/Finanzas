f41eeec4b8634de7e5f9141f05973355a1b448f2
Hart Praise <praise@esettlementgroup.com>
2020-12-15 13:58:19 +0100

    finished up the home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { TopNavComponent } from './components/top-nav/top-nav.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    TopNavComponent,
+    BannerComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
